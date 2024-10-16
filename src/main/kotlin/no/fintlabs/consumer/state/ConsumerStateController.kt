52a6de2602ce408b050e4f6cf958de54f4f2f80e
Hknots <hknots99@gmail.com>
2024-10-16 15:40:49 +0200

    add ConsumerStateController

---
+++ src/main/kotlin/no/fintlabs/consumer/state/ConsumerStateController.kt
@@ -0,0 +1,28 @@
+package no.fintlabs.consumer.state
+
+import no.fintlabs.consumer.state.service.ConsumerStateService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ConsumerStateController(
+    val consumerStateService: ConsumerStateService
+) {
+
+    @GetMapping
+    fun getStates(): Collection<ConsumerState> =
+        consumerStateService.getConsumerStates()
+
+    @PostMapping
+    fun addConsumerState(@RequestBody consumerState: ConsumerState) =
+        consumerStateService.addConsumerState(consumerState)
+
+    @PutMapping("/{id}")
+    fun updateConsumerState(@RequestBody consumerState: ConsumerState, @PathVariable id: String) =
+        consumerStateService.updateConsumerState(consumerState, id)
+
+}
