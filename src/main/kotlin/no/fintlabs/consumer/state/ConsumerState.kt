0a73b908711ccc86e1ef6644d2074cd4c709e3f3
Hknots <hknots99@gmail.com>
2024-10-16 18:19:01 +0200

    convert to Entity

--- src/main/kotlin/no/fintlabs/consumer/state/ConsumerState.kt
+++ src/main/kotlin/no/fintlabs/consumer/state/ConsumerState.kt
@@ -1,10 +1,21 @@
 package no.fintlabs.consumer.state
 
+import jakarta.persistence.ElementCollection
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import java.util.*
+
+@Entity
 data class ConsumerState(
     val domain: String,
     val `package`: String,
     val org: String,
     val version: String,
-    val env: Map<String, String> = mutableMapOf(),
     var managed: Boolean? = true,
-)
+
+    @ElementCollection
+    val writeable: List<String> = mutableListOf()
+) {
+    @Id
+    val id: String = UUID.randomUUID().toString()
+}
