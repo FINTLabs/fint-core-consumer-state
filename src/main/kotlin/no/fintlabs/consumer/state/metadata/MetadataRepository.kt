f7a5a52df0e65185fb11c3fc0d0db6f14de1b3be
Hknots <hknots99@gmail.com>
2024-10-22 23:12:03 +0200

    add Metadata repository & fetcher

---
+++ src/main/kotlin/no/fintlabs/consumer/state/metadata/MetadataRepository.kt
@@ -0,0 +1,31 @@
+package no.fintlabs.consumer.state.metadata
+
+import jakarta.annotation.PostConstruct
+import org.springframework.stereotype.Repository
+import java.util.concurrent.ConcurrentHashMap
+
+@Repository
+class MetadataRepository(
+    val metadataFetcher: MetadataFetcher
+) {
+
+    val componentToResourceMap: ConcurrentHashMap<String, MutableSet<String>> = ConcurrentHashMap()
+
+    @PostConstruct
+    fun populateComponentToResourceMap() =
+        metadataFetcher.getMetadata().subscribe {
+            if (it.packageName != null) {
+                componentToResourceMap.computeIfAbsent(createComponentKey(it.domainName, it.packageName)) { mutableSetOf() }
+                    .add(it.resourceName)
+            }
+        }
+
+    fun containsComponent(domain: String, `package`: String) =
+        componentToResourceMap.containsKey(createComponentKey(domain, `package`))
+
+    fun resourceBelongsToComponent(domain: String, `package`: String, resource: String) =
+        componentToResourceMap.getOrDefault(createComponentKey(domain, `package`), mutableSetOf()).contains(resource)
+
+    private fun createComponentKey(domain: String, `package`: String) = "${domain.lowercase()}-${`package`.lowercase()}"
+
+}
