b24817772867a82d1a5dfa8fef09b3c1c71596d3
Hknots <hknots99@gmail.com>
2024-10-22 23:12:30 +0200

    add webclient config

---
+++ src/main/kotlin/no/fintlabs/consumer/state/config/WebClientConfig.kt
@@ -0,0 +1,15 @@
+package no.fintlabs.consumer.state.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
+
+@Configuration
+class WebClientConfig(
+    @Value("\${fint.metamodel}") private val metamodelUrl: String
+) {
+
+    @Bean("metamodelWebClient")
+    fun metamodelWebClient(): WebClient = WebClient.create(metamodelUrl)
+}
