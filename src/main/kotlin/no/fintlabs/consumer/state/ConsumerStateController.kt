39225e1d60a34027c6fda5e0792a1f208239468a
Hknots <hknots99@gmail.com>
2024-10-22 23:14:20 +0200

    add consumerValidator & add RequestMapping

--- src/main/kotlin/no/fintlabs/consumer/state/ConsumerStateController.kt
+++ src/main/kotlin/no/fintlabs/consumer/state/ConsumerStateController.kt
@@ -1,46 +1,51 @@
 package no.fintlabs.consumer.state
 
-import no.fintlabs.consumer.state.model.ConsumerState
-import no.fintlabs.consumer.state.model.ConsumerStateUpdate
-import no.fintlabs.consumer.state.service.ConsumerStateService
-import org.springframework.http.HttpRequest
+import no.fintlabs.consumer.state.model.ConsumerEntity
+import no.fintlabs.consumer.state.model.ConsumerRequest
+import no.fintlabs.consumer.state.model.ConsumerUpdateRequest
+import no.fintlabs.consumer.state.model.interfaces.ConsumerIdentification
+import no.fintlabs.consumer.state.model.validation.ConsumerValidator
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.http.server.ServerHttpRequest
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 import org.springframework.web.server.ServerWebExchange
 import java.net.URI
 
 @RestController
+@RequestMapping("/consumer-state")
 class ConsumerStateController(
-    val consumerStateService: ConsumerStateService
+    val consumerStateService: ConsumerStateService,
+    val consumerValidator: ConsumerValidator
 ) {
 
     @GetMapping
-    fun getStates(): Collection<ConsumerState> = consumerStateService.getConsumerStates()
+    fun getConsumers(): Collection<ConsumerEntity> = consumerStateService.getConsumers()
 
     @PostMapping
-    fun addConsumerState(@RequestBody consumerState: ConsumerState, request: ServerWebExchange): ResponseEntity<ConsumerState> =
-        consumerStateService.addConsumerState(consumerState).let {
-            ResponseEntity.created(URI.create("${request.request.uri}${it.id}")).body(it)
+    fun addConsumer(
+        @RequestBody consumerRequest: ConsumerRequest,
+        webExchange: ServerWebExchange
+    ): ResponseEntity<ConsumerIdentification> {
+        consumerValidator.validateRequest(consumerRequest)
+        return consumerStateService.saveConsumer(consumerRequest).let {
+            ResponseEntity.created(URI.create("${webExchange.request.uri}${it.domain}")).body(it)
         }
+    }
 
     @PutMapping("/{id}")
-    fun updateConsumerState(@RequestBody consumerStateUpdate: ConsumerStateUpdate, @PathVariable id: String): ConsumerState =
-        consumerStateService.updateConsumerState(consumerStateUpdate, id)
-            .orElseThrow{ ResponseStatusException(HttpStatus.NOT_FOUND) }
+    fun updateConsumer(
+        @PathVariable id: String,
+        @RequestBody consumerUpdateRequest: ConsumerUpdateRequest,
+    ): ConsumerEntity {
+        consumerValidator.validateUpdateRequest(id, consumerUpdateRequest)
+        return consumerStateService.updateConsumer(id, consumerUpdateRequest)
+            .orElseThrow { ResponseStatusException(HttpStatus.NOT_FOUND) }
+    }
 
     @DeleteMapping("/{id}")
-    fun deleteConsumerState(@PathVariable id: String): ConsumerState =
-        consumerStateService.deleteConsumerState(id)
+    fun deleteConsumer(@PathVariable id: String): ConsumerEntity =
+        consumerStateService.deleteConsumer(id)
             .orElseThrow { ResponseStatusException(HttpStatus.NOT_FOUND) }
 
     // TODO: Temporary, get rid of this in production
