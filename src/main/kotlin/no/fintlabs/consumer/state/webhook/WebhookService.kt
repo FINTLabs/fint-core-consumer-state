6b1210544f2d317dc083e878e1421772e627d105
Hknots <hknots99@gmail.com>
2025-01-15 09:07:51 +0100

    add callBack

--- src/main/kotlin/no/fintlabs/consumer/state/webhook/WebhookService.kt
+++ src/main/kotlin/no/fintlabs/consumer/state/webhook/WebhookService.kt
@@ -1,14 +1,20 @@
 package no.fintlabs.consumer.state.webhook
 
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
 
 @Service
-class WebhookService {
-
+class WebhookService(
+    private val webClient: WebClient = WebClient.create(),
     private val callbacks: MutableSet<String> = mutableSetOf()
+) {
 
     fun addCallback(ip: String) = callbacks.add(ip)
 
-    fun getCallbacks() = callbacks.toSet()
+    fun callBack(any: Any) = callbacks.forEach {
+        webClient.post()
+            .uri(it)
+            .body(any, Any::class.java)
+    }
 
 }
