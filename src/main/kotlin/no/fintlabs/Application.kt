f4ffa3bd96b3686e46fed53d76e778e5cab4e23d
Hknots <hknots99@gmail.com>
2025-01-21 14:48:39 +0100

    annotate WebHookServer

--- src/main/kotlin/no/fintlabs/Application.kt
+++ src/main/kotlin/no/fintlabs/Application.kt
@@ -1,11 +1,13 @@
-package no.fintlabs.consumer.state
+package no.fintlabs
 
+import no.fintlabs.webhook.server.annotation.WebHookServer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@WebHookServer
 @SpringBootApplication
 class FintCoreConsumerStateApplication
 
 fun main(args: Array<String>) {
-	runApplication<FintCoreConsumerStateApplication>(*args)
+    runApplication<FintCoreConsumerStateApplication>(*args)
 }
