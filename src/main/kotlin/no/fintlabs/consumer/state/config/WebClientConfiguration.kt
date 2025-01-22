6ae291a4cfc2e400a6f5c14b439568ca2eba6c31
Hknots <hknots99@gmail.com>
2025-01-22 15:04:00 +0100

    implement endpointProperties

--- src/main/kotlin/no/fintlabs/consumer/state/config/WebClientConfiguration.kt
+++ src/main/kotlin/no/fintlabs/consumer/state/config/WebClientConfiguration.kt
@@ -1,15 +1,16 @@
 package no.fintlabs.consumer.state.config
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
-class WebClientConfiguration(
-    @Value("\${fint.metamodel}") private val metamodelUrl: String
-) {
+class WebClientConfiguration(private val endpoints: EndpointProperties) {
 
     @Bean("metamodelWebClient")
-    fun metamodelWebClient(): WebClient = WebClient.create(metamodelUrl)
+    fun metamodelWebClient(): WebClient = WebClient.create(endpoints.metamodel)
+
+    @Bean("adminWebClient")
+    fun adminWebClient(): WebClient = WebClient.create(endpoints.admin)
+
 }
