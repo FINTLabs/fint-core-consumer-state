a45432884165bae2e3be22b38f5832995fc66bc0
Hknots <hknots99@gmail.com>
2025-01-22 15:04:50 +0100

    annotate ConfigurationPropertiesScan

--- src/main/kotlin/no/fintlabs/Application.kt
+++ src/main/kotlin/no/fintlabs/Application.kt
@@ -2,9 +2,11 @@ package no.fintlabs
 
 import no.fintlabs.webhook.server.annotation.WebHookServer
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @WebHookServer
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class FintCoreConsumerStateApplication
 
