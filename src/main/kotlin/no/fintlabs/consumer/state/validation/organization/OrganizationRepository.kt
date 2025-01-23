c810cd9c45e63168266a83b812f65063ff21d739
Hknots <hknots99@gmail.com>
2025-01-23 10:24:29 +0100

    remove logger & move set to constructor

--- src/main/kotlin/no/fintlabs/consumer/state/validation/organization/OrganizationRepository.kt
+++ src/main/kotlin/no/fintlabs/consumer/state/validation/organization/OrganizationRepository.kt
@@ -1,19 +1,16 @@
 package no.fintlabs.consumer.state.validation.organization
 
 import jakarta.annotation.PostConstruct
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
 
 @Repository
 class OrganizationRepository(
-    private val organizationFetcher: OrganizationFetcher
+    private val organizationFetcher: OrganizationFetcher,
+    private val orgNames: MutableSet<String> = mutableSetOf()
 ) {
 
-    private val logger = LoggerFactory.getLogger(this::class.java)
-    private val orgNames: MutableSet<String> = mutableSetOf()
-
     @PostConstruct
-    private fun populateOrgNames() = organizationFetcher.getOrganizations().subscribe{ orgNames.add(it.name) }
+    private fun populateOrgNames() = organizationFetcher.getOrganizations().subscribe { orgNames.add(it.name) }
 
     fun orgExists(org: String) = orgNames.contains(org)
 
