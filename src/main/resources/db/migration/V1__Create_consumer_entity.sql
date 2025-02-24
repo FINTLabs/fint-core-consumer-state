319c412bf7d248e9a17dfd33c88facd910d412a6
Hknots <hknots99@gmail.com>
2025-02-24 14:10:21 +0100

    fix: flatten PodResources fields in schema

--- src/main/resources/db/migration/V1__Create_consumer_entity.sql
+++ src/main/resources/db/migration/V1__Create_consumer_entity.sql
@@ -1,37 +1,16 @@
 CREATE TABLE consumer_entity
 (
-    id              VARCHAR(255) PRIMARY KEY,
-    domain          VARCHAR(255) NOT NULL,
-    package         VARCHAR(255) NOT NULL,
-    org             VARCHAR(255) NOT NULL,
-    version         VARCHAR(255) NOT NULL,
-    shared          BOOLEAN      NOT NULL,
-    limits_cpu      VARCHAR(255),
-    limits_memory   VARCHAR(255),
-    requests_cpu    VARCHAR(255),
-    requests_memory VARCHAR(255)
-);
-
-CREATE TABLE consumer_entity_resources
-(
-    consumer_entity_id VARCHAR(255) NOT NULL,
-    element            VARCHAR(255) NOT NULL,
-    PRIMARY KEY (consumer_entity_id, element),
-    FOREIGN KEY (consumer_entity_id) REFERENCES consumer_entity (id)
-);
-
-CREATE TABLE consumer_entity_writeable_resources
-(
-    consumer_entity_id VARCHAR(255) NOT NULL,
-    element            VARCHAR(255) NOT NULL,
-    PRIMARY KEY (consumer_entity_id, element),
-    FOREIGN KEY (consumer_entity_id) REFERENCES consumer_entity (id)
-);
-
-CREATE TABLE consumer_entity_cache_disabled_resources
-(
-    consumer_entity_id VARCHAR(255) NOT NULL,
-    element            VARCHAR(255) NOT NULL,
-    PRIMARY KEY (consumer_entity_id, element),
-    FOREIGN KEY (consumer_entity_id) REFERENCES consumer_entity (id)
+    id                       VARCHAR(255) PRIMARY KEY,
+    domain                   VARCHAR(255) NOT NULL,
+    "package"                VARCHAR(255) NOT NULL,
+    org                      VARCHAR(255) NOT NULL,
+    version                  VARCHAR(255) NOT NULL,
+    shared                   BOOLEAN      NOT NULL,
+    limits_cpu               VARCHAR(255) NOT NULL,
+    limits_memory            VARCHAR(255) NOT NULL,
+    requests_cpu             VARCHAR(255) NOT NULL,
+    requests_memory          VARCHAR(255) NOT NULL,
+    resources                TEXT[],
+    writeable_resources      TEXT[],
+    cache_disabled_resources TEXT[]
 );
