1880182f57f69e37966ef5b721c79c291aa0ba26
Hknots <hknots99@gmail.com>
2025-02-24 14:09:51 +0100

    feat: use R2dbcEntityTemplate when saving new entities

--- src/main/kotlin/no/fintlabs/consumer/state/ConsumerStateService.kt
+++ src/main/kotlin/no/fintlabs/consumer/state/ConsumerStateService.kt
@@ -9,29 +9,32 @@ import no.fintlabs.consumer.state.model.ConsumerRequest
 import no.fintlabs.consumer.state.model.ConsumerResponse
 import no.fintlabs.consumer.state.model.Operation
 import no.fintlabs.consumer.state.repository.ConsumerEntity
-import no.fintlabs.consumer.state.repository.ConsumerEntity.Companion.createId
 import no.fintlabs.consumer.state.repository.ConsumerEntity.Companion.fromRequest
 import no.fintlabs.consumer.state.repository.ConsumerStateRepository
 import no.fintlabs.webhook.server.WebhookServerService
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
 import org.springframework.stereotype.Service
 
 @Service
 class ConsumerStateService(
     private val consumerStateRepository: ConsumerStateRepository,
-    private val webhookServerService: WebhookServerService
+    private val webhookServerService: WebhookServerService,
+    private val r2dbcEntityTemplate: R2dbcEntityTemplate
 ) {
 
     fun getConsumers(): Flow<ConsumerEntity> = consumerStateRepository.findAll().asFlow()
 
-    suspend fun saveConsumer(consumerRequest: ConsumerRequest): Pair<ConsumerEntity, Boolean> =
-        consumerStateRepository.findById(createId(consumerRequest))
+    suspend fun saveConsumer(consumerRequest: ConsumerRequest): Pair<ConsumerEntity, Boolean> {
+        val id = ConsumerEntity.createId(consumerRequest)
+        return consumerStateRepository.findById(id)
             .awaitSingleOrNull()?.let {
                 it to false
             } ?: run {
-            val entity = consumerStateRepository.save(fromRequest(consumerRequest)).awaitSingle()
+            val entity = r2dbcEntityTemplate.insert(fromRequest(consumerRequest)).awaitSingle()
             webhookCallback(entity, Operation.CREATE)
             entity to true
         }
+    }
 
     suspend fun updateConsumer(id: String, consumerUpdate: ConsumerFields): ConsumerEntity? =
         consumerStateRepository.findById(id).awaitSingleOrNull()?.let { existing ->
